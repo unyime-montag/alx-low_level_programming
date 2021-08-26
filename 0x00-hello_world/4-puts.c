e2f1b690e6a72c3530b29a38708bc682cb700360
unyime-montag <unyime.ekanem@hotmail.com>
2021-08-26 04:06:46 -0700

    commit m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
- * main - main block
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
