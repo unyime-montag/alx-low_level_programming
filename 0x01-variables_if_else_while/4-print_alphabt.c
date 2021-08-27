bf4cf2a85d32b0721ffe9e73b9e98593a3132f97
unyime-montag <unyime.ekanem@hotmail.com>
2021-08-27 11:19:29 -0700

    a commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
- * Return: Aways 0 (Sucess)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char ch = 'a';
+char low, e, q;
 
-while (ch <= 'z')
+e = 'e';
+q = 'q';
+
+for (low = 'a'; low <= 'z'; low++)
 {
-if (ch != 'e' && ch != 'q')
-putchar(ch);
+if (low != e && low != q)
+putchar(low);
 }
-ch++;
-}
-putchar('\n');
+printf("\n");
+
 return (0);
 }
