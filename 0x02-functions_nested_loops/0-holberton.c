b517916f276153b5c829c9302ba05d006fcb7cbd
unyime-montag <unyime.ekanem@hotmail.com>
2021-08-31 11:11:48 -0700

    a test

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,18 @@
 #include "holberton.h"
-
 /**
- * main -  prints Holberton, followed by a new line
- * Return: Always 0 (Success)
+ * main - Entry point
+ * write a program tha prints Holberton, followed by a new line.
+ * Return: Always (Success)
  */
 int main(void)
 {
-  _putchar('H');
-  _putchar('o');
-  _putchar('l');
-  _putchar('b');
-  _putchar('e');
-  _putchar('r');
-  _putchar('t');
-  _putchar('o');
-  _putchar('n');
-  _putchar('\n');
+  char *ch = "_putchar";
 
+  while (*ch)
+    {
+      _putchar(*ch);
+      ch++;
+    }
+  _putchar('\n');
   return (0);
-}ex
+}
