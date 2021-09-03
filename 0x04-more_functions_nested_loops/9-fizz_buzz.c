3518646cbbe4c4f161c9c5c0d24ef2a8f4fbd5a5
unyime-montag <unyime.ekanem@hotmail.com>
2021-09-03 14:11:54 -0700

    a neee

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -7,29 +6,29 @@
  */
 int main(void)
 {
-  int n;
+int n;
 
-  n = 1;
-  printf("%d", n);
-  for (n = 2; n <= 100; n++)
-    {
-      if ((n % 3 == 0) && (n % 5 == 0))
-	{
-	  printf(" FizzBuzz");
-	}
-      else if (n % 3 == 0)
-	{
-	  printf(" Fizz");
-	}
-      else if (n % 5 == 0)
-	{
-	  printf(" Buzz");
-	}
-      else
-	{
-	  printf(" %d", n);
-	}
-    }
-  printf("\n");
-  return (0);
+n = 1;
+printf("%d", n);
+for (n = 2; n <= 100; n++)
+{
+if ((n % 3 == 0) && (n % 5 == 0))
+{
+printf(" FizzBuzz");
+}
+else if (n % 3 == 0)
+{
+printf(" Fizz");
+}
+else if (n % 5 == 0)
+{
+printf(" Buzz");
+}
+else
+{
+printf(" %d", n);
+}
+}
+printf("\n");
+return (0);
 }
