75e32e87a25cef7aa7d22fea0b893e7757ed6273
unyime-montag <unyime.ekanem@hotmail.com>
2021-09-07 12:37:59 -0700

    go

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print a string to stdout
+ * @str: string
+ * Description: use _putchar
+ */
+
+void _puts(char *str)
+{
+  int i;
+  11;rgb:0000/0000/0000
+       for (i = 0; str[i] != '\0'; i++)
+	 {
+	   _putchar(str[i]);
+	 }
+  _putchar('\n');
+}
