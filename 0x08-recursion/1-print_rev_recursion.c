87dfa669cba23623faa01ce99f62ff154c28c625
unyime-montag <unyime.ekanem@hotmail.com>
2021-09-15 13:28:27 -0700

    do

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+11;rgb:0000/0000/0000/**
+		      * _print_rev_recursion - prints string in reverse
+		      * @s: pointer to the string
+		      * Return: void
+		      */
+
+void _print_rev_recursion(char *s)
+{
+  if (*s > '\0')
+    {
+      _print_rev_recursion(s + 1);
+      _putchar(*s);
+    }
+
+}
