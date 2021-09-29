65c763326fe9c87204c40711d4be4d9c3c29e910
unyime-montag <unyime.ekanem@hotmail.com>
2021-09-28 20:23:10 -0700

    push

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,18 +9,17 @@
  */
 void print_dog(struct dog *d)
 {
-  if (d)
-    {
-      if (!(d->name))
-	printf("Name: (nil)\n");
-      else
-	printf("Name: %s\n", d->name);
+if (d)
+{
+if (!(d->name))
+printf("Name: (nil)\n");
+else
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
 
-      printf("Age: %f\n", d->age);
-
-      if (!(d->owner))
-	printf("Owner: (nil)\n");
-      else
-	printf("Owner: %s\n", d->owner);
-    }
+if (!(d->owner))
+printf("Owner: (nil)\n");
+else
+printf("Owner: %s\n", d->owner);
+}
 }
